0498adc84b71779fbc014f3e4a6f7bf63c02fc8e
niko057 <nicatnamazov57.nn@gmail.com>
2024-05-14 11:12:56 +0400

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC1.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Nicat";
+            ViewBag.Surname = "Namazov";
+            ViewBag.Age = "27";
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = "+994107179757";
+            return View();
+        }
+    }
+}
